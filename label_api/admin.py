86f8392e4adf8b66eb53268b9ac90be4ed4673e5
tunal <ethemtunal@gmail.com>
2021-08-31 02:57:01 +0300

    Register to models

--- label_api/admin.py
+++ label_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Image, Annotator, Annotation, Consensus
 
-# Register your models here.
+admin.site.register(Image)
+admin.site.register(Annotator)
+admin.site.register(Annotation)
+admin.site.register(Consensus)
