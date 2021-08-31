19719b0028d65f4a93fac62912ec7c55a8d7c4dc
tunal <ethemtunal@gmail.com>
2021-08-31 03:03:44 +0300

    Add ImageDetailAPIView with methods

--- label_api/views.py
+++ label_api/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from .models import Image, Annotator, Annotation, Consensus
 from .serializers import ImageSerializer, AnnotatorSerializer, AnnotationSerializer, ConsensusSerializer
 from rest_framework.response import Response
@@ -19,3 +20,30 @@ class ImageAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ImageDetailAPIView(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Image.objects.filter(uuid=pk)
+        except Image.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, pk):
+        image = self.get_object(pk)
+        serializer = ImageSerializer(image, many=True)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        image = self.get_object(pk)
+        serializer = ImageSerializer(image.first(), data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        image = self.get_object(pk)
+        image.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
