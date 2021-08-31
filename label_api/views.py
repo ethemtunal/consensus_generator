bf116b23c50e97b821d7fa26d5ef518a1f67c9f5
tunal <ethemtunal@gmail.com>
2021-08-31 03:02:37 +0300

    Add ImageAPIView with methods

--- label_api/views.py
+++ label_api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from .models import Image, Annotator, Annotation, Consensus
+from .serializers import ImageSerializer, AnnotatorSerializer, AnnotationSerializer, ConsensusSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class ImageAPIView(APIView):
+
+    def get(self, request):
+        images = Image.objects.all()
+        serializer = ImageSerializer(images, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ImageSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
